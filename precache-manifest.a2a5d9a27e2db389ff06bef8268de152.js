5bc23c6eab1cf8b6eef3292bcbff7a17f96a93ff
unknown <zubairsamo36@gmail.com>
2019-11-30 03:58:15 +0500

    Updates

---
+++ precache-manifest.a2a5d9a27e2db389ff06bef8268de152.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "9f70ac01d0dbf125be82",
+    "url": "/portfolio/static/js/main.9f70ac01.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "9f70ac01d0dbf125be82",
+    "url": "/portfolio/static/css/main.75cd0476.chunk.css"
+  },
+  {
+    "revision": "9d9c0620e62b1a8c2faee2f2bc9152cb",
+    "url": "/portfolio/index.html"
+  }
+];
